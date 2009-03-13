b1a4b32cbb4f8595f81fcffe06a8e9360463b1d6
kvasov <kvasov@5ef0ceb3-152f-2d47-87cb-444b2ffc6d3a>
2009-03-13 08:54:41 +0000

    Создаю нормальный layout

---
+++ src/PrgData.Common/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+namespace PrgData.Common
+{
+	public class Settings
+	{
+		public static string ReadOnlyConnectionString()
+		{ 
+#if DEBUG 	
+			//return "Database=usersettings;Data Source=testsql.analit.net;User Id=system;Password=newpass;pooling=true";
+			return ConfigurationManager.ConnectionStrings["ReadOnly"].ConnectionString;
+#else		
+			return ConfigurationManager.ConnectionStrings["DB"].ReadOnlyConnectionString;
+#endif
+		}
+
+
+		public static string ReadWriteConnectionString()
+		{
+#if DEBUG
+			//return "Database=usersettings;Data Source=testsql.analit.net;User Id=system;Password=newpass;pooling=true";
+			return ConfigurationManager.ConnectionStrings["ReadWrite"].ConnectionString;
+#else		
+			return ConfigurationManager.ConnectionStrings["DB"].ReadWriteConnectionString;
+#endif
+		}
+
+	}
+}
