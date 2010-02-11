bbd6b47262600fc008369dbb0b4027b5569fe585
morozov <morozov@5ef0ceb3-152f-2d47-87cb-444b2ffc6d3a>
2010-02-11 05:59:36 +0000

    Скорректировал ProcedureFixture, чтобы тесты более корректнее работали. Произвел рефакторинг OrderHelperFixture.

--- src/Integration/OrderHelperFixture.cs
+++ src/Integration/OrderHelperFixture.cs
@@ -5,6 +5,7 @@ using System.Text;
 using NUnit.Framework;
 using MySql.Data.MySqlClient;
 using PrgData.Common;
+using System.Data;
 
 namespace Integration
 {
@@ -12,8 +13,32 @@ namespace Integration
 	public class OrderHelperFixture
 	{
 
+		private DataSet GetActivePrices(MySqlConnection connection, UpdateData updateData)
+		{
+			MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.Prices");
+			MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.ActivePrices");
+			if (updateData.IsFutureClient)
+				MySqlHelper.ExecuteNonQuery(connection, "call future.GetActivePrices(" + updateData.ClientId + ")");
+			else
+				MySqlHelper.ExecuteNonQuery(connection, "call usersettings.GetActivePrices(" + updateData.ClientId + ")");
+			return MySqlHelper.ExecuteDataset(connection, "select * from usersettings.ActivePrices limit 1");
+		}
+
+		private void CheckOrder(ulong orderId, MySqlConnection connection, Action action)
+		{
+			Assert.That(orderId > 0, "не получилось сохранить заказ");
+			try
+			{
+				action();
+			}
+			finally
+			{
+				MySqlHelper.ExecuteNonQuery(connection, "delete from orders.OrdersHead where RowId = " + orderId);
+			}
+		}
+
 		[Test(Description = "отправляем заказы под старым клиентом и проверяем время прайс-листа")]
-		public void send_order_by_old_client()
+		public void check_PriceDate_in_order_by_old_client()
 		{
 			using (var connection = new MySqlConnection(Settings.ConnectionString()))
 			{
@@ -21,10 +46,7 @@ namespace Integration
 				//Пользователь "sergei" - это клиент с кодом 1349, он должен быть старым
 				var updateData = UpdateHelper.GetUpdateData(connection, "sergei");
 				var helper = new OrderHelper(updateData, connection, connection);
-				MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.Prices");
-				MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.ActivePrices");
-				MySqlHelper.ExecuteNonQuery(connection, "call usersettings.GetActivePrices(" + updateData.ClientId + ")");
-				var dsPrice = MySqlHelper.ExecuteDataset(connection, "select * from usersettings.ActivePrices limit 1");
+				var dsPrice = GetActivePrices(connection, updateData);
 				var sendPrice = dsPrice.Tables[0].Rows[0];
 				var orderid = helper.SaveOrder(
 					updateData.ClientId, 
@@ -34,22 +56,18 @@ namespace Integration
 					1, 
 					1, 
 					null);
-				Assert.That(orderid > 0, "не получилось сохранить заказ");
-				try
+
+				CheckOrder(orderid, connection, () =>
 				{
 					var dsOrder = MySqlHelper.ExecuteDataset(connection, "select * from orders.OrdersHead where RowId = " + orderid);
 					var drOrder = dsOrder.Tables[0].Rows[0];
 					Assert.That(drOrder["PriceDate"], Is.EqualTo(Convert.ToDateTime(sendPrice["PriceDate"])), "не совпадает дата прайс-листа в заказе с датой прайс-листа");
-				}
-				finally
-				{
-					MySqlHelper.ExecuteNonQuery(connection, "delete from orders.OrdersHead where RowId = " + orderid);
-				}
+				});
 			}
 		}
 
 		[Test(Description = "отправляем заказы под клиентом из 'новой реальности' и проверяем время прайс-листа")]
-		public void send_order_by_future_client()
+		public void check_PriceDate_in_order_by_future_client()
 		{
 			using (var connection = new MySqlConnection(Settings.ConnectionString()))
 			{
@@ -57,10 +75,7 @@ namespace Integration
 				//Пользователь "10081" - это пользователь, привязанный к клиенту с кодом 10005, который должен быть клиентом из "Новой реальности"
 				var updateData = UpdateHelper.GetUpdateData(connection, "10081");
 				var helper = new OrderHelper(updateData, connection, connection);
-				MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.Prices");
-				MySqlHelper.ExecuteNonQuery(connection, "drop temporary table if exists Usersettings.ActivePrices");
-				MySqlHelper.ExecuteNonQuery(connection, "call future.GetActivePrices(" + updateData.ClientId + ")");
-				var dsPrice = MySqlHelper.ExecuteDataset(connection, "select * from usersettings.ActivePrices limit 1");
+				var dsPrice = GetActivePrices(connection, updateData);
 				var sendPrice = dsPrice.Tables[0].Rows[0];
 				var orderid = helper.SaveOrder(
 					updateData.ClientId,
@@ -70,17 +85,13 @@ namespace Integration
 					1,
 					1,
 					null);
-				Assert.That(orderid > 0, "не получилось сохранить заказ");
-				try
+
+				CheckOrder(orderid, connection, () =>
 				{
 					var dsOrder = MySqlHelper.ExecuteDataset(connection, "select * from orders.OrdersHead where RowId = " + orderid);
 					var drOrder = dsOrder.Tables[0].Rows[0];
 					Assert.That(drOrder["PriceDate"], Is.EqualTo(Convert.ToDateTime(sendPrice["PriceDate"])), "не совпадает дата прайс-листа в заказе с датой прайс-листа");
-				}
-				finally
-				{
-					MySqlHelper.ExecuteNonQuery(connection, "delete from orders.OrdersHead where RowId = " + orderid);
-				}
+				});
 			}
 		}
 	}
