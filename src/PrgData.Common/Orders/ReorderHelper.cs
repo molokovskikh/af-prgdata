02b3f5c57d8dc7092a6ced4c39e4f771b7a1250f
morozov <morozov@5ef0ceb3-152f-2d47-87cb-444b2ffc6d3a>
2010-06-21 22:09:34 +0000

    Backport изменений из ветки kill-include-stable-with-MinReq-and-EnableUpdate ревизии 427: Ревизия: 427 Автор: morozov Дата: 14:11:14, 4 июня 2010 г. Сообщение: В отладочной сборке документы генерируются только для заказов без ошибок. ---- Изменено : /branches/kill-include-stable-with-MinReq-and-EnableUpdate/src/PrgData.Common/Orders/ReorderHelper.cs

--- src/PrgData.Common/Orders/ReorderHelper.cs
+++ src/PrgData.Common/Orders/ReorderHelper.cs
@@ -86,7 +86,7 @@ namespace PrgData.Common.Orders
 
 #if DEBUG
 						if ((_data.ClientId == 1349) || (_data.ClientId == 10005))
-							GenerateDocsHelper.GenerateDocs(_readWriteConnection, _data, _orders);
+							GenerateDocsHelper.GenerateDocs(_readWriteConnection, _data, _orders.FindAll(item => item.SendResult == OrderSendResult.Success));
 #endif
 
 						transaction.Commit();
