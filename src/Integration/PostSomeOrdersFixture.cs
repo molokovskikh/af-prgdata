c86bdb6eab226fb7344885435183eabde7173b1c
Morozov Sergey <morozov@analit.net>
2010-09-14 15:29:25 +0400

    Новшество #2931 При сохранении заказов из 705-й версии пытаться получить детализацию заказываемой позиции из Core
    переделал тест на автоматически генерируемого пользователя.

--- src/Integration/PostSomeOrdersFixture.cs
+++ src/Integration/PostSomeOrdersFixture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using Castle.ActiveRecord;
 using Castle.MicroKernel.Registration;
 using Common.Models;
 using Common.Models.Tests.Repositories;
@@ -14,6 +15,7 @@ using System.Data;
 using PrgData.Common;
 using SmartOrderFactory.Domain;
 using SmartOrderFactory.Repositories;
+using Test.Support;
 
 
 namespace Integration
@@ -21,9 +23,21 @@ namespace Integration
 	[TestFixture]
 	public class PostSomeOrdersFixture
 	{
-		[Test]
-		public void Send_orders_without_SupplierPriceMarkup()		
+		private TestOldClient oldClient;
+		private TestOldUser oldUser;
+
+
+		private bool getOffers;
+		private DataRow activePrice;
+		private DataRow firstOffer;
+		private DataRow secondOffer;
+		private DataRow thirdOffer;
+		private DataRow fourOffer;
+
+		[SetUp]
+		public void Setup()
 		{
+
 			Test.Support.Setup.Initialize();
 			ContainerInitializer.InitializerContainerForTests(typeof(SmartOrderRule).Assembly);
 			IoC.Container.Register(
@@ -31,79 +45,285 @@ namespace Integration
 				);
 
 			ServiceContext.GetUserHost = () => "127.0.0.1";
-			ServiceContext.GetUserName = () => "sergei";
 			ConfigurationManager.AppSettings["WaybillPath"] = "FtpRoot\\";
 			if (Directory.Exists("FtpRoot"))
 				FileHelper.DeleteDir("FtpRoot");
 			Directory.CreateDirectory("FtpRoot");
-			CreateFolders("1349");
 
+			using (var transaction = new TransactionScope())
+			{
+				var permission = TestUserPermission.ByShortcut("AF");
 
-			using(var connection = new MySqlConnection(PrgData.Common.Settings.ConnectionString()))
+				oldClient = TestOldClient.CreateTestClient();
+				oldUser = oldClient.Users[0];
+
+				ServiceContext.GetUserName = () => oldUser.OSUserName;
+
+				var session = ActiveRecordMediator.GetSessionFactoryHolder().CreateSession(typeof(ActiveRecordBase));
+				try
+				{
+					session.CreateSQLQuery(@"
+				insert into usersettings.AssignedPermissions (PermissionId, UserId) values (:permissionid, :userid)")
+						.SetParameter("permissionid", permission.Id)
+						.SetParameter("userid", oldUser.Id)
+						.ExecuteUpdate();
+				}
+				finally
+				{
+					ActiveRecordMediator.GetSessionFactoryHolder().ReleaseSession(session);
+				}
+			}
+
+			CreateFolders(oldClient.Id.ToString());
+
+			MySqlHelper.ExecuteNonQuery(Settings.ConnectionString(), @"
+delete 
+from orders.OrdersHead 
+where 
+    ClientCode = ?ClientCode 
+and WriteTime > now() - interval 2 week"
+				,
+				new MySqlParameter("?ClientCode", oldClient.Id));
+
+			GetOffers();
+		}
+
+		private void GetOffers()
+		{
+			if (!getOffers)
+				using (var connection = new MySqlConnection(Settings.ConnectionString()))
+				{
+					connection.Open();
+
+					MySqlHelper.ExecuteNonQuery(
+						connection,
+						@"
+drop temporary table if exists Usersettings.Prices, Usersettings.ActivePrices, Usersettings.Core;
+call usersettings.GetOffers(?ClientCode, 0)",
+						new MySqlParameter("?ClientCode", oldClient.Id));
+
+					activePrice = ExecuteDataRow(
+						connection, @"
+select 
+* 
+from 
+  ActivePrices 
+where 
+  (select Count(*) from Core where Core.PriceCode = ActivePrices.PriceCode and Core.RegionCode = ActivePrices.RegionCode) > 3000 limit 1");
+
+					var firstProductId = MySqlHelper.ExecuteScalar(
+						connection,
+						@"
+select 
+  c.ProductId 
+from 
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+group by c.ProductId
+having count(distinct c.SynonymCode) > 2
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]));
+
+					var secondProductId = MySqlHelper.ExecuteScalar(
+						connection,
+						@"
+select 
+  c.ProductId 
+from 
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId <> ?FirstProductId
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?FirstProductId", firstProductId));
+
+					var thirdProductId = MySqlHelper.ExecuteScalar(
+						connection,
+						@"
+select 
+  c.ProductId 
+from 
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId <> ?FirstProductId
+and C.ProductId <> ?SecondProductId
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?FirstProductId", firstProductId),
+						new MySqlParameter("?SecondProductId", secondProductId));
+
+					firstOffer = ExecuteDataRow(
+						connection,
+						@"
+select
+  Core.Cost,
+  Core.RegionCode,
+  c.*
+from
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId = ?ProductId
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?ProductId", firstProductId));
+					Assert.IsNotNull(firstOffer, "Не найдено предложение");
+
+					secondOffer = ExecuteDataRow(
+						connection,
+						@"
+select
+  Core.Cost,
+  Core.RegionCode,
+  c.*
+from
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId = ?ProductId
+and C.SynonymCode <> ?SynonymCode
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?ProductId", firstProductId),
+						new MySqlParameter("?SynonymCode", firstOffer["SynonymCode"]));
+					Assert.IsNotNull(secondOffer, "Не найдено предложение");
+
+					thirdOffer = ExecuteDataRow(
+						connection,
+						@"
+select
+  Core.Cost,
+  Core.RegionCode,
+  c.*
+from
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId = ?ProductId
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?ProductId", secondProductId));
+					Assert.IsNotNull(thirdOffer, "Не найдено предложение");
+
+					fourOffer = ExecuteDataRow(
+						connection,
+						@"
+select
+  Core.Cost,
+  Core.RegionCode,
+  c.*
+from
+  Core 
+  inner join farm.Core0 c on c.Id = Core.Id
+where
+    Core.PriceCode = ?PriceCode
+and Core.RegionCode = ?RegionCode
+and C.ProductId = ?ProductId
+limit 1
+"
+						,
+						new MySqlParameter("?PriceCode", activePrice["PriceCode"]),
+						new MySqlParameter("?RegionCode", activePrice["RegionCode"]),
+						new MySqlParameter("?ProductId", thirdProductId));
+					Assert.IsNotNull(fourOffer, "Не найдено предложение");
+					getOffers = true;
+				}
+		}
+
+		private static DataRow ExecuteDataRow(MySqlConnection connection, string commandText, params MySqlParameter[] parms)
+		{
+			DataSet set = MySqlHelper.ExecuteDataset(connection, commandText, parms);
+			if (set == null)
+			{
+				return null;
+			}
+			if (set.Tables.Count == 0)
+			{
+				return null;
+			}
+			if (set.Tables[0].Rows.Count == 0)
+			{
+				return null;
+			}
+			return set.Tables[0].Rows[0];
+		}
+
+		[Test]
+		public void Send_orders_without_SupplierPriceMarkup()		
+		{
+			using(var connection = new MySqlConnection(Settings.ConnectionString()))
 			{
 				connection.Open();
 
-				var command = new MySqlCommand();
-				command.Connection = connection;
-
-				var UniqueId = MySqlHelper.ExecuteScalar(connection, "select AFCopyId from usersettings.UserUpdateInfo where UserId = " + 1289).ToString();
-
-				command.CommandText = "call usersettings.GetOffers(1349, 0);";
-				command.ExecuteNonQuery();
-
-				command.CommandText = @"
-select
-  *
-from
-  ActivePrices ap,
-  Core c,
-  farm.Core0 fc
-where
-    c.PriceCode = ap.PriceCode
-and c.RegionCode = ap.RegionCode
-and fc.Id = c.Id
-and fc.ProductId = c.ProductId
-limit 10";
-
-				var dataAdapter = new MySqlDataAdapter(command);
-				var positions = new DataTable();
-				dataAdapter.Fill(positions);
-				var position = positions.Rows[0];
+				var UniqueId = MySqlHelper.ExecuteScalar(connection, "select AFCopyId from usersettings.UserUpdateInfo where UserId = " + oldUser.Id).ToString();
 
 				string serverResponse;
 				using (var prgData = new PrgDataEx())
 				{
 					serverResponse = prgData.PostSomeOrders(
 						UniqueId,
-						true,
-						false,
-						1349u,
-						1,
-						new ulong[] { 1L },
-						new ulong[] { Convert.ToUInt64(position["PriceCode"]) },
-						new ulong[] { Convert.ToUInt64(position["RegionCode"]) },
-						new DateTime[] { Convert.ToDateTime(position["PriceDate"]) },
-						new string[] { "" },
-						new ushort[] { 1 },
-						new ulong[] { 1 },
-						new ulong[] { Convert.ToUInt64(position["Id"]) },
-						new ulong[] { Convert.ToUInt64(position["ProductId"]) },
-						new string[] { position["CodeFirmCr"].ToString() },
-						new ulong[] { Convert.ToUInt64(position["SynonymCode"]) },
-						new string[] { position["SynonymFirmCrCode"].ToString() },
-						new string[] { position["Code"].ToString() },
-						new string[] { position["Code"].ToString() },
-						new bool[] { Convert.ToBoolean(position["Junk"]) },
-						new bool[] { Convert.ToBoolean(position["Await"]) },
-						new string[] { position["RequestRatio"].ToString() },
-						new string[] { position["OrderCost"].ToString() },
-						new string[] { position["MinOrderCount"].ToString() },
-						new ushort[] { 1 },
-						new decimal[] { Convert.ToDecimal(position["Cost"]) },
-						new string[] { "" },
-						new string[] { "" },
-						new string[] { "" },
-						new string[] { "" });
+						true,         //ForceSend
+						false,        //UseCorrectOrders
+						oldClient.Id, //ClientId
+						1,            //OrderCount
+						new ulong[] { 1L },  //ClientOrderId
+						new ulong[] { Convert.ToUInt64(activePrice["PriceCode"]) },
+						new ulong[] { Convert.ToUInt64(activePrice["RegionCode"]) },
+						new DateTime[] { DateTime.Now }, //PriceDate
+						new string[] { "" },             //ClientAddition
+						new ushort[] { 1 },              //RowCount
+						new ulong[] { 1 },               //ClientPositionId
+						new ulong[] { Convert.ToUInt64(firstOffer["Id"].ToString().Substring(firstOffer["Id"].ToString().Length - 9, 9)) },  //ClientServerCoreId
+						new ulong[] { Convert.ToUInt64(firstOffer["ProductId"]) },
+						new string[] { firstOffer["CodeFirmCr"].ToString() },
+						new ulong[] { Convert.ToUInt64(firstOffer["SynonymCode"]) },
+						new string[] { firstOffer["SynonymFirmCrCode"].ToString() },
+						new string[] { firstOffer["Code"].ToString() },
+						new string[] { firstOffer["Code"].ToString() },
+						new bool[] { Convert.ToBoolean(firstOffer["Junk"]) },
+						new bool[] { Convert.ToBoolean(firstOffer["Await"]) },
+						new string[] { firstOffer["RequestRatio"].ToString() },
+						new string[] { firstOffer["OrderCost"].ToString() },
+						new string[] { firstOffer["MinOrderCount"].ToString() },
+						new ushort[] { 1 }, //Quantity
+						new decimal[] { Convert.ToDecimal(firstOffer["Cost"]) },
+						new string[] { firstOffer["Cost"].ToString() },        //minCost
+						new string[] { activePrice["PriceCode"].ToString() },  //MinPriceCode
+						new string[] { firstOffer["Cost"].ToString() },        //leaderMinCost
+						new string[] { activePrice["PriceCode"].ToString() }); //leaderMinPriceCode
 				}
 
 				Assert.That(serverResponse, Is.Not.Null);
@@ -122,19 +342,23 @@ limit 10";
 				Assert.That(serverOrderId, Is.Not.Null);
 				Assert.That(serverOrderId, Is.Not.Empty);
 
-				try
-				{
-					var realOrderId = MySqlHelper.ExecuteScalar(connection, "select RowId from orders.ordershead where RowId = " + serverOrderId);
-					Assert.That(realOrderId, Is.Not.Null);
-					Assert.That(realOrderId.ToString(), Is.Not.Empty);
+				var realOrderId = MySqlHelper.ExecuteScalar(connection, "select RowId from orders.ordershead where RowId = " + serverOrderId);
+				Assert.That(realOrderId, Is.Not.Null);
+				Assert.That(realOrderId.ToString(), Is.Not.Empty);
 
-					var supplierPriceMarkup = MySqlHelper.ExecuteScalar(connection, "select SupplierPriceMarkup from orders.ordersList where OrderId = " + serverOrderId);
-					Assert.That(supplierPriceMarkup, Is.EqualTo(DBNull.Value));
-				}
-				finally
-				{
-					MySqlHelper.ExecuteNonQuery(connection, "delete from orders.ordershead where RowId = " + serverOrderId);
-				}
+				var supplierPriceMarkup = MySqlHelper.ExecuteScalar(connection, "select SupplierPriceMarkup from orders.ordersList where OrderId = " + serverOrderId);
+				Assert.That(supplierPriceMarkup, Is.EqualTo(DBNull.Value));
+
+				var orderedOffersId = MySqlHelper.ExecuteScalar(connection, @"
+select 
+OrderedOffers.Id 
+from 
+orders.ordersList 
+left join orders.OrderedOffers on OrderedOffers.Id = ordersList.RowId
+where ordersList.OrderId = " + serverOrderId);
+				Assert.That(orderedOffersId, Is.Not.Null);
+				Assert.That(orderedOffersId, Is.Not.EqualTo(DBNull.Value));
+
 			}
 		}
 
