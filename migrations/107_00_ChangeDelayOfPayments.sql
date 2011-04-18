7974f70d68bbbfccc460cd8d26f3f3799bdd2789
Morozov Sergey <morozov@analit.net>
2011-04-19 00:49:15 +0400

    Добавил уникальный ключ в таблицу DelayOfPayments.

--- migrations/107_00_ChangeDelayOfPayments.sql
+++ migrations/107_00_ChangeDelayOfPayments.sql
@@ -5,7 +5,8 @@
   SupplierId int unsigned not null,
   `VitallyImportantDelay` decimal(5,3) NOT NULL DEFAULT '0.000' comment 'Значение отсрочки платежа для ЖНВЛС',
   `OtherDelay` decimal(5,3) NOT NULL DEFAULT '0.000' comment 'Значение отсрочки платежа для прочего ассортимента',
-  `DayOfWeek` enum ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') not null, 
+  `DayOfWeek` enum ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') not null,   
   primary key (Id),
+  UNIQUE KEY `DelayOfPaymentId` (`ClientId`,`SupplierId`, `DayOfWeek`),
   CONSTRAINT `FK_DelayOfPayments_SupplierId` FOREIGN KEY (`SupplierId`) REFERENCES usersettings.`ClientsData` (`FirmCode`) ON DELETE CASCADE ON UPDATE CASCADE
 );
