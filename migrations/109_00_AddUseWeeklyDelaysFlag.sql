3dd296b445f382ccd0554e77b1ee2a311a24d9c9
Morozov Sergey <morozov@analit.net>
2011-04-20 11:49:51 +0400

    Новшество #3773 Модернизация отсрочки платежа
    добавил настройку "Использовать отсрочки платежа, установленные для каждого дня недели", устанавливаемую клиентом для каждого поставщика.

---
+++ migrations/109_00_AddUseWeeklyDelaysFlag.sql
@@ -0,0 +1,2 @@
+﻿alter table `usersettings`.`SupplierIntersection`
+  add column UseWeeklyDelays tinyint(1) unsigned not null default '0' comment 'Использовать отсрочки платежа, установленные для каждого дня недели';
