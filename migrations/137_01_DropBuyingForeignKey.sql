44bbfd1be90d923931a78f3f7daa3599590b9a13
Morozov Sergey <morozov@analit.net>
2011-05-18 09:32:34 +0400

    Добавил миграции по требованию Ошибка #3669 Обработка матрицы закупок (для Фармаимпекса).

---
+++ migrations/137_01_DropBuyingForeignKey.sql
@@ -0,0 +1,13 @@
+﻿truncate farm.BuyingMatrix;
+
+alter table farm.BuyingMatrix
+  drop FOREIGN KEY FK_BuyingMatrix_CatalogId,
+  drop FOREIGN KEY FK_BuyingMatrix_PriceId,
+  drop FOREIGN KEY FK_BuyingMatrix_ProducerId;
+
+alter table farm.BuyingMatrix
+  drop key FK_BuyingMatrix_Comb;
+
+alter table farm.BuyingMatrix
+  drop key FK_BuyingMatrix_CatalogId,
+  drop key FK_BuyingMatrix_ProducerId;
---
+++ migrations/137_03_AddProductIdToByuingMatrix.sql
@@ -0,0 +1,13 @@
+﻿alter table farm.BuyingMatrix
+  add column `ProductId` int unsigned not null AFTER `PriceId`;
+
+alter table farm.BuyingMatrix
+  drop column CatalogId;
+
+alter table farm.BuyingMatrix
+  add constraint FK_BuyingMatrix_PriceId foreign key (PriceId) references usersettings.PricesData(PriceCode) on delete cascade,
+  add constraint FK_BuyingMatrix_ProductId foreign key (ProductId) references catalogs.Products(Id) on delete cascade,
+  add constraint FK_BuyingMatrix_ProducerId foreign key (ProducerId) references catalogs.Producers(Id) on delete set null;
+
+alter table farm.BuyingMatrix
+  add UNIQUE KEY `FK_BuyingMatrix_Comb` (`PriceId`,`ProductId`,`ProducerId`) USING BTREE;
