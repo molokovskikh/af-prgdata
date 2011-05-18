563a6e5909a96cad1318c4bb43ff0357aba26446
Morozov Sergey <morozov@analit.net>
2011-05-18 10:59:47 +0400

    Ошибка #3669 Обработка матрицы закупок (для Фармаимпекса)
    добавил заполнение матрицы закупок для прайс-листа 4957.

---
+++ migrations/137_03_FillByuingMatrixTo4957.sql
@@ -0,0 +1,8 @@
+﻿delete from farm.BuyingMatrix
+where priceId = 4957;
+
+insert into farm.BuyingMatrix(PriceId, Code, ProductId, ProducerId)
+select c0.PriceCode, c0.Code, c0.ProductId, c0.CodeFirmCr
+from farm.Core0 c0
+where pricecode = 4957
+group by c0.ProductId, c0.CodeFirmCr;
