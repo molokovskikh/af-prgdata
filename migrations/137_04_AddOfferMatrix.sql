6d3b89109a652c75ee81048fa55db40012c9011a
Morozov Sergey <morozov@analit.net>
2011-05-18 12:41:13 +0400

    Новшество #4239 Матрица предложений: формирование сервисом подготовки данных предложений на основе прайс-листа
    добавил миграцию.

---
+++ migrations/137_04_AddOfferMatrix.sql
@@ -0,0 +1,4 @@
+﻿alter table usersettings.RetClientsSet
+  add column `OfferMatrixPriceId` int unsigned default null comment 'ссылка на ассортиментный прайс-лист, из которого будет формироваться матрица предложений, если не установлен, то механизм не активирован',
+  add column `OfferMatrixType` int unsigned not null default 0 comment 'тип матрицы предложений: 0 - белый список, 1 - черный список',
+  add constraint FK_RetClientsSet_OfferMatrixPriceId foreign key (OfferMatrixPriceId) references usersettings.PricesData(PriceCode) on delete set null;
