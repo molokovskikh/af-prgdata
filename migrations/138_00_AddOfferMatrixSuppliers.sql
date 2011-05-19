39cde7257f217c14c5ce2e84f2661f6875a15ee7
Morozov Sergey <morozov@analit.net>
2011-05-19 11:34:51 +0400

    Новшество #4239 Матрица предложений: формирование сервисом подготовки данных предложений на основе прайс-листа
    добавил миграцию.

---
+++ migrations/138_00_AddOfferMatrixSuppliers.sql
@@ -0,0 +1,9 @@
+﻿create table UserSettings.OfferMatrixSuppliers
+(
+  Id int unsigned not null auto_increment,
+  ClientId int unsigned not null,
+  SupplierId int unsigned not null,
+  primary key (Id),
+  CONSTRAINT `FK_OfferMatrixSuppliers_ClientId` FOREIGN KEY (ClientId) REFERENCES future.Clients (Id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_OfferMatrixSuppliers_SupplierId` FOREIGN KEY (SupplierId) REFERENCES future.Suppliers (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
