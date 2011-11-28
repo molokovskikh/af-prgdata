ba9d125fd7059997b6ab3039148e86cbdda57a31
Morozov Sergey <morozov@analit.net>
2011-11-28 15:47:50 +0400

    Создал event, удаляющий старые записи из PrgDataLogs по расписанию.

---
+++ migrations/332_00_AddDeleteOldLockEvent.sql
@@ -0,0 +1,10 @@
+﻿CREATE 
+	DEFINER = 'RootDBMS'@'127.0.0.1'
+EVENT logs.DeleteOldLocks
+	ON SCHEDULE EVERY '15' MINUTE
+	STARTS '2011-11-28 15:30:00'
+	ON COMPLETION PRESERVE
+	DO 
+BEGIN
+  delete from Logs.PrgDataLogs where StartTime < now() - interval 20 minute;
+END;
