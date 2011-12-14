f2e0a0fea2ddc2de30d2feb2d0ed03ce41ba983a
Morozov Sergey <morozov@analit.net>
2011-12-14 12:41:57 +0400

    Новшество #7079 Мини.почта (Получение сообщений от поставщиков)
    добавил таблицу для подтверждения доставки писем от поставщиков.

---
+++ migrations/348_00_AddMailSendLogs.sql
@@ -0,0 +1,11 @@
+﻿create table logs.MailSendLogs(
+  Id int unsigned not null auto_increment,
+  UserId int unsigned not null,
+  MailId int unsigned not null,
+  UpdateId int unsigned default null,
+  Committed tinyint(1) unsigned NOT NULL DEFAULT '0',
+  primary key (Id),
+  CONSTRAINT `FK_MailSendLogs_UserId` FOREIGN KEY (`UserId`) REFERENCES `future`.`users` (`Id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_MailSendLogs_MailId` FOREIGN KEY (`MailId`) REFERENCES `Documents`.`Mails` (Id) ON DELETE CASCADE,
+  CONSTRAINT `FK_MailSendLogs_UpdateId` FOREIGN KEY (`UpdateId`) REFERENCES `logs`.`analitfupdates` (`UpdateId`) ON DELETE CASCADE
+);
