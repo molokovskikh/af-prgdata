ac3f3d49574e0eb6407a8e464845217e51cb55a0
Morozov Sergey <morozov@analit.net>
2012-02-08 15:31:15 +0400

    Ошибка #7477 Навести порядок в версионности автообновления АФ

---
+++ migrations/404_00_On7477DeleteOldVersions.sql
@@ -0,0 +1,148 @@
+#delete (19, 1229, 1325)
+delete from usersettings.analitfversionrules where Id = 19;
+#delete (21, 1259, 1325)
+delete from usersettings.analitfversionrules where Id = 21;
+#delete (23, 1263, 1325)
+delete from usersettings.analitfversionrules where Id = 23;
+#delete (25, 1269, 1325)
+delete from usersettings.analitfversionrules where Id = 25;
+#delete (27, 1271, 1325)
+delete from usersettings.analitfversionrules where Id = 27;
+#delete (29, 1285, 1325)
+delete from usersettings.analitfversionrules where Id = 29;
+#delete (31, 1289, 1325)
+delete from usersettings.analitfversionrules where Id = 31;
+#delete (33, 1295, 1325)
+delete from usersettings.analitfversionrules where Id = 33;
+#delete (247, 1297, 1298)
+delete from usersettings.analitfversionrules where Id = 247;
+#delete (35, 1299, 1325)
+delete from usersettings.analitfversionrules where Id = 35;
+#delete (37, 1315, 1325)
+delete from usersettings.analitfversionrules where Id = 37;
+#delete (189, 1315, 1363)
+delete from usersettings.analitfversionrules where Id = 189;
+#delete (41, 1317, 1325)
+delete from usersettings.analitfversionrules where Id = 41;
+#delete (191, 1317, 1363)
+delete from usersettings.analitfversionrules where Id = 191;
+#delete (43, 1321, 1325)
+delete from usersettings.analitfversionrules where Id = 43;
+#delete (193, 1321, 1363)
+delete from usersettings.analitfversionrules where Id = 193;
+#delete (45, 1323, 1325)
+delete from usersettings.analitfversionrules where Id = 45;
+#delete (195, 1323, 1363)
+delete from usersettings.analitfversionrules where Id = 195;
+#delete (199, 1326, 1363)
+delete from usersettings.analitfversionrules where Id = 199;
+#delete (201, 1349, 1363)
+delete from usersettings.analitfversionrules where Id = 201;
+#delete (203, 1353, 1363)
+delete from usersettings.analitfversionrules where Id = 203;
+#delete (205, 1357, 1363)
+delete from usersettings.analitfversionrules where Id = 205;
+#delete (215, 1359, 1363)
+delete from usersettings.analitfversionrules where Id = 215;
+#delete (259, 1364, 1385)
+delete from usersettings.analitfversionrules where Id = 259;
+#delete (238, 1385, 1411)
+delete from usersettings.analitfversionrules where Id = 238;
+#delete (255, 1385, 1431)
+delete from usersettings.analitfversionrules where Id = 255;
+#delete (240, 1403, 1411)
+delete from usersettings.analitfversionrules where Id = 240;
+#delete (242, 1407, 1411)
+delete from usersettings.analitfversionrules where Id = 242;
+#delete (363, 1409, 1413)
+delete from usersettings.analitfversionrules where Id = 363;
+#delete (421, 1410, 1414)
+delete from usersettings.analitfversionrules where Id = 421;
+#delete (257, 1411, 1431)
+delete from usersettings.analitfversionrules where Id = 257;
+#delete (277, 1412, 1413)
+delete from usersettings.analitfversionrules where Id = 277;
+#delete (265, 1431, 1461)
+delete from usersettings.analitfversionrules where Id = 265;
+#delete (267, 1431, 1489)
+delete from usersettings.analitfversionrules where Id = 267;
+#delete (271, 1431, 1493)
+delete from usersettings.analitfversionrules where Id = 271;
+#delete (269, 1461, 1489)
+delete from usersettings.analitfversionrules where Id = 269;
+#delete (273, 1461, 1493)
+delete from usersettings.analitfversionrules where Id = 273;
+#delete (275, 1489, 1493)
+delete from usersettings.analitfversionrules where Id = 275;
+#delete (315, 1493, 1529)
+delete from usersettings.analitfversionrules where Id = 315;
+#delete (317, 1517, 1529)
+delete from usersettings.analitfversionrules where Id = 317;
+#delete (319, 1519, 1529)
+delete from usersettings.analitfversionrules where Id = 319;
+#delete (321, 1521, 1529)
+delete from usersettings.analitfversionrules where Id = 321;
+#delete (327, 1527, 1529)
+delete from usersettings.analitfversionrules where Id = 327;
+#delete (351, 1529, 1531)
+delete from usersettings.analitfversionrules where Id = 351;
+#delete (354, 1529, 1533)
+delete from usersettings.analitfversionrules where Id = 354;
+#delete (356, 1529, 1553)
+delete from usersettings.analitfversionrules where Id = 356;
+#delete (353, 1531, 1533)
+delete from usersettings.analitfversionrules where Id = 353;
+#delete (358, 1531, 1553)
+delete from usersettings.analitfversionrules where Id = 358;
+#delete (360, 1533, 1553)
+delete from usersettings.analitfversionrules where Id = 360;
+#delete (362, 1551, 1553)
+delete from usersettings.analitfversionrules where Id = 362;
+#delete (373, 1551, 1567)
+delete from usersettings.analitfversionrules where Id = 373;
+#delete (371, 1553, 1567)
+delete from usersettings.analitfversionrules where Id = 371;
+#delete (393, 1567, 1572)
+delete from usersettings.analitfversionrules where Id = 393;
+#delete (395, 1567, 1573)
+delete from usersettings.analitfversionrules where Id = 395;
+#delete (385, 1567, 1591)
+delete from usersettings.analitfversionrules where Id = 385;
+#delete (397, 1567, 1600)
+delete from usersettings.analitfversionrules where Id = 397;
+#delete (399, 1567, 1601)
+delete from usersettings.analitfversionrules where Id = 399;
+#delete (401, 1567, 1604)
+delete from usersettings.analitfversionrules where Id = 401;
+#delete (403, 1567, 1605)
+delete from usersettings.analitfversionrules where Id = 403;
+#delete (413, 1567, 1606)
+delete from usersettings.analitfversionrules where Id = 413;
+#delete (415, 1567, 1607)
+delete from usersettings.analitfversionrules where Id = 415;
+#delete (417, 1567, 1610)
+delete from usersettings.analitfversionrules where Id = 417;
+#delete (405, 1600, 1604)
+delete from usersettings.analitfversionrules where Id = 405;
+#delete (407, 1600, 1605)
+delete from usersettings.analitfversionrules where Id = 407;
+#delete (409, 1601, 1604)
+delete from usersettings.analitfversionrules where Id = 409;
+#delete (411, 1601, 1605)
+delete from usersettings.analitfversionrules where Id = 411;
+#delete (420, 1607, 1611)
+delete from usersettings.analitfversionrules where Id = 420;
+#delete (423, 1611, 1635)
+delete from usersettings.analitfversionrules where Id = 423;
+#delete (425, 1611, 1647)
+delete from usersettings.analitfversionrules where Id = 425;
+#delete (429, 1611, 1649)
+delete from usersettings.analitfversionrules where Id = 429;
+#delete (435, 1611, 1651)
+delete from usersettings.analitfversionrules where Id = 435;
+#delete (427, 1635, 1647)
+delete from usersettings.analitfversionrules where Id = 427;
+#delete (431, 1635, 1649)
+delete from usersettings.analitfversionrules where Id = 431;
+#delete (433, 1647, 1649)
+delete from usersettings.analitfversionrules where Id = 433;
