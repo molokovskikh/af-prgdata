c00119aa94a67e4acb42924557c7a9c871c2c93c
Квасов Роман <r.kvasov@analit.net>
2014-09-15 13:06:22 +0400

    Новшество #27421 Доработка функционала автозаказа
    Исправил ошибки

--- src/PrgData.Common/SmartOrderHelper.cs
+++ src/PrgData.Common/SmartOrderHelper.cs
@@ -206,12 +206,6 @@ namespace PrgData.Common
 						item.RowId = _maxOrderListId;
 						_maxOrderListId++;
 
-						//var report = list.Find(reportItem => { return reportItem.Item != null && reportItem.Item.OrderItem == item; });
-
-						//var cryptCostWithoutDelayOfPayment =
-//							report.Item.Offer.CostWithoutDelayOfPayment.ToString(CultureInfo.InvariantCulture.NumberFormat);
-						//var cryptCost = report.Item.Offer.Cost.ToString(CultureInfo.InvariantCulture.NumberFormat);
-
 						buildItems.AppendFormat(
 							"{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}\t{19}\n",
 							item.RowId,
